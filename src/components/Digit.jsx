cf26de3caa5bbecdc9fd86a6cf48a485c083e17c
joseph <heegu0311@gmail.com>
2025-11-25 12:54:42 +0900

    test: remove gsap for iphone 5

--- src/components/Digit.jsx
+++ src/components/Digit.jsx
@@ -1,6 +1,4 @@
 import React, { useRef, useState } from "react";
-import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
 
 const Digit = ({ digit }) => {
   const prevDigit = digit - 1 > 0 ? digit - 1 : 9;
@@ -10,53 +8,26 @@ const Digit = ({ digit }) => {
   const [flip, setFlip] = useState(prevDigit);
   const container = useRef(null);
 
-  useGSAP(
-    async () => {
-      setUpper(digit);
-
-      await gsap.fromTo(
-        ".upper.flip",
-        { rotateX: 0 },
-        {
-          rotateX: -90,
-          duration: 0.35,
-          repeat: 0,
-          ease: "linear",
-        },
-      );
-      setState(true);
-      setFlip(digit);
-      await gsap.fromTo(
-        ".lower.flip",
-        { rotateX: -90 },
-        { rotateX: 0, duration: 0.35, repeat: 0, ease: "linear" },
-      );
-      setState(false);
-      setLower(digit);
-    },
-    { scope: container, dependencies: [digit] },
-  );
-
   return (
     <div ref={container} className="num">
       <div className="upper">
         <div>
-          <span>{upper}</span>
+          <span>{digit}</span>
         </div>
       </div>
       <div className="upper flip">
         <div>
-          <span>{flip}</span>
+          <span>{digit}</span>
         </div>
       </div>
       <div className="lower flip" style={{ display: state ? "block" : "none" }}>
         <div>
-          <span>{flip}</span>
+          <span>{digit}</span>
         </div>
       </div>
       <div className="lower">
         <div>
-          <span>{lower}</span>
+          <span>{digit}</span>
         </div>
       </div>
     </div>
